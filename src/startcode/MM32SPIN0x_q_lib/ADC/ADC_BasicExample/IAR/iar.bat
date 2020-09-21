777d0a5f71996ab3d33b08032dfe43ccaaa4bd0d
qitas <qitas@qitas.cn>
2020-09-21 09:42:54 +0800

    fix adc

---
+++ src/startcode/MM32SPIN0x_q_lib/ADC/ADC_BasicExample/IAR/iar.bat
@@ -0,0 +1,33 @@
+del *.bin /s
+del *.lst /s
+del *.txt /s
+del *.ewt /s
+del *.dep /s
+del *.bak /s
+del *.ddk /s
+del *.edk /s
+del *.lst /s
+del *.lnp /s
+del *.mpf /s
+del *.mpj /s
+del *.obj /s
+del *.omf /s
+del *.plg /s
+del *.rpt /s
+del *.tmp /s
+del *.__i /s
+del *.crf /s
+del *.o /s
+del *.d /s
+del *.axf /s
+del *.tra /s      
+del *.iex /s
+del *.htm /s
+del *.sct /s
+del *.map /s
+rd settings /s /q
+rd Debug /s /q
+rd list /s /q
+rd Lst /s /q
+rd DebugConfig /s /q
+exit
